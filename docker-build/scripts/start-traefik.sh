6e240f399a87f7d94e69410535efe0c6f576f9c6
goflutterjava <goflutterjava@gmail.com>
2022-12-27 16:20:00 +0800

    enable dashboard

--- docker-build/scripts/start-traefik.sh
+++ docker-build/scripts/start-traefik.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p $TRAEFIK_HOME/logs
-$TRAEFIK_HOME/traefik >>$TRAEFIK_HOME/logs/apisix.stdout.log 2>>$TRAEFIK_HOME/logs/apisix.stderr.log &
+$TRAEFIK_HOME/traefik --api=true --api.insecure=true >>$TRAEFIK_HOME/logs/apisix.stdout.log 2>>$TRAEFIK_HOME/logs/apisix.stderr.log &
